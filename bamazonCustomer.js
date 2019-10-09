bb2389c1222425c0cdcde3f8eb21ecd02f3ed031
Carlo Ortega <cfor0@outlook.com>
2019-10-09 11:21:31 -0400

    Added first inquirer question, working on second

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,17 +16,65 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId)
     runSearch();
 });
+const displayItems = () => {
+    var query = "SELECT * FROM products";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log(`
+ID: ${res}
+        `)
+    })
+}
+const runSearch = () => {
 
-const runSearch = function {
     inquirer
         .prompt({
-//ask them the ID of the product they would like to buy
-            
-//ask how many units of the product they would like to buy
+            //ask them the ID of the product they would like to buy
+            name: "action",
+            type: "list",
+            message: "What is the id of the item you would like to buy?",
+            choices: [
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+            ]
         })
-        .then(answers => {
-
-        }
+        .then(function (answer) {
+            console.log(answer.action);
+            var query = "SELECT * FROM products WHERE ?";
+            connection.query(query, { id: answer.action }, function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    console.log(`
+Item selected:   ${res[i].title}
+Quantity:        ${res[i].quantity} `)
+                }
+                unitAmount();
+            });
+        });
 }
+
+
+
+const unitAmount = () => {
+    //ask how many units of the product they would like to buy
+    inquirer
+        .prompt({
+            name: "amount",
+            type: "input",
+            message: "How many units would you like?(ENTER A NUMBER)",
+
+        })
+        .then(function (answer) {
+            var query = "SELECT quantity FROM products";
+            connection.query(query, function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+
+                }
+            });
+        });
+}
+
+
+
 
 
